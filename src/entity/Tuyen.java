433ba47bc9ee94fc0bcffbb814b89f5dc38d949b
PhamKhaVy <23632971.vy@student.iuh.edu.vn>
2025-09-25 15:43:25 +0700

    Rang buoc tuyen

--- src/entity/Tuyen.java
+++ src/entity/Tuyen.java
@@ -12,10 +12,101 @@ package entity;
  * @version: 1.0
  */
 
+import java.util.Objects;
+
 public class Tuyen {
 	private String tuyenID;
 	private Ga gaDi;
 	private Ga gaDen;
 	private int khoangCachKm;
 	private int thoiGianDuKienPhut;
+
+	public Tuyen() {
+		super();
+	}
+
+	public Tuyen(String tuyenID, Ga gaDi, Ga gaDen, int khoangCachKm, int thoiGianDuKienPhut) {
+		this.tuyenID = tuyenID;
+		this.gaDi = gaDi;
+		this.gaDen = gaDen;
+		this.khoangCachKm = khoangCachKm;
+		this.thoiGianDuKienPhut = thoiGianDuKienPhut;
+	}
+
+	public String getTuyenID() {
+		return tuyenID;
+	}
+
+	public void setTuyenID(String tuyenID) {
+		if(tuyenID == null || tuyenID.isEmpty()) {
+			throw new IllegalArgumentException("TuyenID khong duoc de trong");
+		}
+		this.tuyenID = tuyenID;
+	}
+
+	public Ga getGaDi() {
+		return gaDi;
+	}
+
+	public void setGaDi(Ga gaDi) {
+		if (gaDi == null) {
+			throw new IllegalArgumentException("Ga di khong duoc de trong");
+		}
+		this.gaDi = gaDi;
+	}
+
+	public Ga getGaDen() {
+		return gaDen;
+	}
+
+	public void setGaDen(Ga gaDen) {
+		if (gaDen == null) {
+			throw new IllegalArgumentException("Ga den khong duoc de trong");
+		}
+		this.gaDen = gaDen;
+	}
+
+	public int getKhoangCachKm() {
+		return khoangCachKm;
+	}
+
+	public void setKhoangCachKm(int khoangCachKm) {
+		if (khoangCachKm <= 0) {
+			throw new IllegalArgumentException("Khoang cach phai lon hon 0");
+		}
+		this.khoangCachKm = khoangCachKm;
+	}
+
+	public int getThoiGianDuKienPhut() {
+		return thoiGianDuKienPhut;
+	}
+
+	public void setThoiGianDuKienPhut(int thoiGianDuKienPhut) {
+		if (thoiGianDuKienPhut <= 0) {
+			throw new IllegalArgumentException("Thoi gian du kien phai lon hon 0");
+		}
+		this.thoiGianDuKienPhut = thoiGianDuKienPhut;
+	}
+
+	@Override
+	public String toString() {
+		return tuyenID + ";"
+				+ gaDi + ";"
+				+ gaDen + ";"
+				+ khoangCachKm + ";"
+				+ thoiGianDuKienPhut;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Tuyen tuyen = (Tuyen) o;
+		return Objects.equals(tuyenID, tuyen.tuyenID);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(tuyenID);
+	}
 }
