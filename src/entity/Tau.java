e1d7c33d41af798509000df97e9656eda8b1d21c
NGUYEN-THI-HUYNH-NHU <huynhnhu.connect@gmail.com>
2025-09-25 16:07:49 +0700

    [Them] constructor/methods vao cac class entity (lan 3)

--- src/entity/Tau.java
+++ src/entity/Tau.java
@@ -20,4 +20,59 @@ public class Tau {
 	private LoaiTau loaiTau;
 	private int soLuongToa;
 	private String trangThai;
+	
+	public Tau(String tauID, String tenTau, LoaiTau loaiTau, int soLuongToa, String trangThai) {
+		super();
+		this.tauID = tauID;
+		this.tenTau = tenTau;
+		this.loaiTau = loaiTau;
+		this.soLuongToa = soLuongToa;
+		this.trangThai = trangThai;
+	}
+
+	public String getTauID() {
+		return tauID;
+	}
+
+	public String getTenTau() {
+		return tenTau;
+	}
+
+	public LoaiTau getLoaiTau() {
+		return loaiTau;
+	}
+
+	public int getSoLuongToa() {
+		return soLuongToa;
+	}
+
+	public String getTrangThai() {
+		return trangThai;
+	}
+
+	public void setTauID(String tauID) {
+		this.tauID = tauID;
+	}
+
+	public void setTenTau(String tenTau) {
+		this.tenTau = tenTau;
+	}
+
+	public void setLoaiTau(LoaiTau loaiTau) {
+		this.loaiTau = loaiTau;
+	}
+
+	public void setSoLuongToa(int soLuongToa) {
+		this.soLuongToa = soLuongToa;
+	}
+
+	public void setTrangThai(String trangThai) {
+		this.trangThai = trangThai;
+	}
+
+	@Override
+	public String toString() {
+		return tauID + ";" + tenTau + ";" + loaiTau + ";" + soLuongToa
+				+ ";" + trangThai;
+	}	
 }
--- src/entity/Toa.java
+++ src/entity/Toa.java
@@ -22,4 +22,59 @@ public class Toa {
 	private HangToa hangToa;
 	private int sucChua;
 	private int sttToa;
+	
+	public Toa(String toaID, Tau tau, HangToa hangToa, int sucChua, int sttToa) {
+		super();
+		this.toaID = toaID;
+		this.tau = tau;
+		this.hangToa = hangToa;
+		this.sucChua = sucChua;
+		this.sttToa = sttToa;
+	}
+
+	public String getToaID() {
+		return toaID;
+	}
+
+	public Tau getTau() {
+		return tau;
+	}
+
+	public HangToa getHangToa() {
+		return hangToa;
+	}
+
+	public int getSucChua() {
+		return sucChua;
+	}
+
+	public int getSttToa() {
+		return sttToa;
+	}
+
+	public void setToaID(String toaID) {
+		this.toaID = toaID;
+	}
+
+	public void setTau(Tau tau) {
+		this.tau = tau;
+	}
+
+	public void setHangToa(HangToa hangToa) {
+		this.hangToa = hangToa;
+	}
+
+	public void setSucChua(int sucChua) {
+		this.sucChua = sucChua;
+	}
+
+	public void setSttToa(int sttToa) {
+		this.sttToa = sttToa;
+	}
+
+	@Override
+	public String toString() {
+		return toaID + ";" + tau + ";" + hangToa + ";" + sucChua + ";"
+				+ sttToa;
+	}
 }
