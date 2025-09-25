9fd50b2875540e39249d59c14db958cac684c0be
PhamKhaVy <23632971.vy@student.iuh.edu.vn>
2025-09-25 11:01:22 +0700

    Test

---
+++ src/entity/PhuongThucThanhToan.java
@@ -0,0 +1,16 @@
+package entity;/*
+ * @ (#) PhuongThucThanhToan.java   1.0     25/09/2025
+package entity;
+
+
+/**
+ * @description :
+ * @author : Vy, Pham Kha Vy
+ * @version 1.0
+ * @created : 25/09/2025
+ */
+
+public class PhuongThucThanhToan {
+    private String phuongThucThanhToanID;
+    private String tenPhuongThuc;
+}
