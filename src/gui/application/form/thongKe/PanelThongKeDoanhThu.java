ded89222b5cb71b7232a5cf68ca1eb0939149b9a
HonDung-77 <23639641.dung@student.iuh.edu.vn>
2025-10-29 07:07:23 +0700

    Dashboard

--- src/gui/application/form/thongKe/PanelThongKeDoanhThu.java
+++ src/gui/application/form/thongKe/PanelThongKeDoanhThu.java
@@ -16,7 +16,6 @@ public class PanelThongKeDoanhThu extends JPanel {
         tab.addTab("Tuyến", new PanelThongKeDoanhThuTheoTuyen());
         tab.addTab("Nhân viên", new PanelThongKeDoanhThuTheoNhanVien());
         tab.addTab("Thanh toán", new PanelThongKeDoanhThuTheoThanhToan());
-        tab.addTab("Dashboard", new PanelThongKeDoanhThuTheoDashboard());
 
 
 
