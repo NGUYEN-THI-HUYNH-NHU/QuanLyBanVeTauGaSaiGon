a89b259aecf28afb4be49e5ba15dea6ddb2b1df1
PhamKhaVy <23632971.vy@student.iuh.edu.vn>
2025-12-09 14:56:29 +0700

    ConnectDB

---
+++ src/gui/application/form/quanLyChuyen/PanelQuanLyChuyen.java
@@ -0,0 +1,14 @@
+/*
+ * @ (#) PanelQuanLyChuyen.java   1.0     26/11/2025
+package gui.application.form.quanLyChuyen;
+
+
+/**
+ * @description :
+ * @author : Vy, Pham Kha Vy
+ * @version 1.0
+ * @created : 26/11/2025
+ */
+
+public class PanelQuanLyChuyen {
+}
