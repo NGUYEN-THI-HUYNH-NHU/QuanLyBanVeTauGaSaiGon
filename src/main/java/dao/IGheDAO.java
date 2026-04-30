8b85946716950e6169d06586a8981ddbfa81d1bf
NGUYEN-THI-HUYNH-NHU <huynhnhu.connect@gmail.com>
2026-04-30 14:45:10 +0700

    [demo]: GheDAO voi Hibernate

--- src/main/java/dao/IGheDAO.java
+++ src/main/java/dao/IGheDAO.java
@@ -6,9 +6,7 @@ import java.util.List;
 
 public interface IGheDAO extends IGenericDAO<Ghe, String> {
     List<Ghe> getGheByGaDiGaDenChuyenToa(String gaDiID, String gaDenID, String chuyenID, String toaID);
-
-    Ghe getGheByChuyenIDGheID(String chuyenID, String gheID);
-
+    
     int calcGia(String chuyenID, String gaDiID, String gaDenID, String loaiTauID, String hangToaID);
 
 }
